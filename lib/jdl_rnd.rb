e1ec3324e6a17934ecf480ad27a00e93c06c02d4
detrix42 <detrix42@gmail.com>
2021-08-26 23:24:56 -0400

    initial commit again

---
+++ lib/jdl_rnd.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative "jdl_rnd/version"
+
+class Error < StandardError; end
+
+class RND
+  attr_reader :seed
+  attr_accessor :range
+
+  def initialize(rng = 6, sd: 0.0)
+    @range = rng
+    if sd == 0.0
+      makeSeedFromTime
+    end
+  end
+
+  def next_seed
+    s = (3.1415926**@seed) - Math.log10(@seed)
+    s = -s if s < 0
+    s *= 1_000_000
+    @seed = s - s.to_i
+  end
+
+  def roll(r = -1)
+    next_seed
+    q = if r.positive?
+      @seed * r
+    else
+      @seed * @range
+        end
+    q.to_i
+  end
+
+  # def =
+  #   next_seed
+  #   (@seed * @range).to_i
+  # end
+
+  def makeSeedFromTime
+    t = Time.now.to_f
+    cnt = 2
+
+    while t > 100
+      t /= 100
+      cnt += 1
+    end
+
+    s = 0.0
+    while cnt != 0
+      s += t.to_i
+      s /= 100
+      t = t - t.to_i
+      t *= 100
+      cnt -= 1
+    end
+    @seed = s
+  end
+end
+
